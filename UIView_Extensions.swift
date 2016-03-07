ab9e9532583b3b4095226fbcaa335aaf699f4388
Hikarus Guijarro-Fayard <ikiteruningen@gmail.com>
2016-03-07 15:08:52 -0500

    Update UIView_Extensions.swift

--- UIView_Extensions.swift
+++ UIView_Extensions.swift
@@ -1,7 +1,13 @@
 /*
-					Hikarus Guijarro-Fayard - iOS developer specialized in Swift
-					http://ikiteruningen.net
-					https://github.com/HikarusGF
+                             .-----------------------------------------------------------------------------.
+                            /  .-.                                                                      .-.  \
+                           |  /   \    Hikarus Guijarro-Fayard - iOS developer specialized in Swift    /   \  |
+                           | |\_.  |                     http://ikiteruningen.net                     |    /| |
+                           |\|  | /|                    https://github.com/HikarusG                   |\  | |/|
+                           | `---' |                                                                  | `---' |
+                           \       |------------------------------------------------------------------|       /
+                            \     /                                                                    \     /
+                             `---'                                                                      `---'
 */
 
 extension UIView {
